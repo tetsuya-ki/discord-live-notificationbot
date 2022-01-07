b7eb860c9e8e68b9fd9b934bd5ca519bc46566ec
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2022-01-08 01:55:45 +0900

    動画名を記載するように変更

--- cogs/livenotificationcog.py
+++ cogs/livenotificationcog.py
@@ -60,10 +60,10 @@ class LiveNotificationCog(commands.Cog):
                                 message = f'''{notification['name']}で{live['title']}さん{message_suffix}\n動画名: {video_title}'''
                             elif result_dict.get('live_streaming_start_flg') is True:
                                 # YouTubeで予約配信していたものが配信開始された場合を想定
-                                message = f'''{notification['name']}で{live['title']}さんの配信が開始されました(おそらく)！'''
+                                message = f'''{notification['name']}で{live['title']}さんの配信が開始されました(おそらく)！\n動画名: {video_title}'''
                             elif result_dict.get('live_streaming_start_flg') is False:
                                 # YouTubeで予約配信が追加された場合を想定
-                                message = f'''{notification['name']}で{live['title']}さんの予約配信が追加されました！'''
+                                message = f'''{notification['name']}で{live['title']}さんの予約配信が追加されました！\n動画名: {video_title}'''
                             description = f'''{result_dict.get('description')} by {live['title']}'''
 
                             # フィルター処理
