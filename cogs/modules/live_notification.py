f095cfaaaae119c7ed5ad5f8c4a526ccb47e3b62
tetsuya-ki <64536338+tetsuya-ki@users.noreply.github.com>
2022-06-05 15:29:23 +0900

    通知時の謎の削除チェックを一部変更
    - 配信前が登録されていた場合は先に進むという条件を削除

--- cogs/modules/live_notification.py
+++ cogs/modules/live_notification.py
@@ -588,8 +588,8 @@ class LiveNotification:
                     if started_at_text != '':
                         dt_started_utc = datetime.datetime.fromisoformat(started_at_text)
                         dt_jst = dt_started_utc.astimezone(self.JST)
-                        # DBの最近の更新日時の方がxmlの最新よりも新しい場合は、削除か何かと判断し、対応しない(配信前が登録されていた場合は先に進む)
-                        if recent_updated_at >= dt_jst and recent_movie_length != 0:
+                        # DBの最近の更新日時の方がxmlの最新よりも新しい場合は、削除か何かと判断し、対応しない
+                        if recent_updated_at >= dt_jst:
                             return
 
                     # 動画が追加されたか、前回確認時に動画の長さが0だった場合のみ、pytubeでYouTube Objectを作成し、動画の長さを取得(長さが0なら未配信とみなす)
@@ -598,6 +598,7 @@ class LiveNotification:
                         or (recent_id == youtube_recent_id and recent_movie_length == 0):
                         youtube_recent_length = 0
                         try:
+                            LOG.info(f'try youtube_recent_id:{youtube_recent_id}')
                             youtube = YouTube(youtube_recent_url)
                             youtube_recent_length = youtube.length
                             if youtube_recent_length == 0:
